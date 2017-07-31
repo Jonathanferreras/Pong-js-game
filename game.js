629d9bed84cbcc97c9efce129f292833377d1a3a
jonathanferreras <jonathanferreras@icloud.com>
2017-07-31 17:57:16 -0400

    made the ball bounce off canvas boundries

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 var canvas;
 var canvas_context;
 var ball_x = 50;
+var ball_y = 50;
+var ball_speed_x = 10;
+var ball_speed_y = 4;
+
 
 window.onload = function () {
   console.log("loaded");
@@ -10,7 +14,7 @@ window.onload = function () {
   var frames_per_second = 30;
 
   setInterval(function() {
-    drawEverything();
+    drawElements();
     moveBall();
   }, 1000 / frames_per_second);
 }
@@ -18,25 +22,39 @@ window.onload = function () {
 function drawBoard() {
   canvas_context.fillStyle = 'black';
   canvas_context.fillRect(0, 0, canvas.width, canvas.height);
-  console.log('Board created.');
 }
 
-function drawPaddle() {
+function drawPaddle(pos_x, pos_y, width, height) {
   canvas_context.fillStyle = 'white';
-  canvas_context.fillRect(0, 210, 10, 100);
+  canvas_context.fillRect(pos_x, pos_y, width, height);
 }
 
 function drawBall() {
   canvas_context.fillStyle = 'white';
-  canvas_context.fillRect(ball_x, 200, 10, 10);
+  canvas_context.beginPath();
+  canvas_context.arc(ball_x, ball_y, 10, 0, Math.PI * 2, true);
+  canvas_context.fill();
 }
 
-function drawEverything() {
+function drawElements() {
   drawBoard();
-  drawPaddle();
+  //Left paddle
+  drawPaddle(10, 105, 20, 70);
+
+  //Right paddle
+  drawPaddle(canvas.width - 35, 105, 20, 70);
   drawBall();
 }
 
 function moveBall() {
-  ball_x = ball_x + 5;
+  ball_x = ball_x + ball_speed_x;
+  ball_y = ball_y + ball_speed_y;
+
+  if (ball_x < 0 || ball_x > canvas.width) {
+    ball_speed_x = -ball_speed_x;
+  }
+
+  if (ball_y < 0 || ball_y > canvas.height) {
+    ball_speed_y = -ball_speed_y;
+  }
 }
